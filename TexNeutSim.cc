95e7826b45d6e9ec5e9303db24f9322ae3ca6b34
KRis <haversonkris@gmail.com>
2024-09-25 21:50:16 +0100

    fixed vis issues. set run vis.mac as default

--- TexNeutSim.cc
+++ TexNeutSim.cc
@@ -25,49 +25,57 @@
 
 
 #include "G4UImanager.hh"
-#include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
+#include "G4VisExecutive.hh"
 #include "G4RunManagerFactory.hh"
 #include "Randomize.hh"
-#include "G4SteppingVerbose.hh"
 #include "DetectorConstruction.hh"
 #include "PhysicsList.hh"
 #include "ActionInitialization.hh"
 
 int main(int argc, char** argv) {
-
+    // Detect interactive mode (if no arguments) and define UI session
     G4UIExecutive* ui = nullptr;
-    if (argc == 1) ui = new G4UIExecutive(argc, argv);
-
-    G4Random::setTheEngine(new CLHEP::RanecuEngine);
-
-    G4int precision = 4;
-    G4SteppingVerbose::UseBestUnit(precision);
-
-    auto runManager = G4RunManagerFactory::CreateRunManager();
-    if (argc == 3) {
-        G4int nThreads = G4UIcommand::ConvertToInt(argv[2]);
-        runManager->SetNumberOfThreads(nThreads);
+    if (argc == 1) {
+        ui = new G4UIExecutive(argc, argv);  // Interactive mode if no arguments
     }
 
-    auto* det = new DetectorConstruction;
+    // Choose the random engine
+    G4Random::setTheEngine(new CLHEP::RanecuEngine);
+
+    // Initialize the run manager
+    auto runManager = G4RunManagerFactory::CreateRunManager();
+
+    // Set mandatory initialization classes
+    DetectorConstruction* det = new DetectorConstruction;
     runManager->SetUserInitialization(det);
 
-    auto* phys = new PhysicsList;
+    PhysicsList* phys = new PhysicsList;
     runManager->SetUserInitialization(phys);
 
     runManager->SetUserInitialization(new ActionInitialization(det));
 
-    // Initialize visualization
+    // Initialize the visualization manager
     G4VisManager* visManager = nullptr;
-    G4UImanager* UImanager = G4UImanager::GetUIpointer();
-
     if (ui) {
         visManager = new G4VisExecutive;
         visManager->Initialize();
+    }
+
+    // Get the pointer to the User Interface manager
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+    // If interactive mode, execute vis.mac and then start the UI session
+    if (ui) {
+        // Execute vis.mac in interactive mode
+        G4String command = "/control/execute vis.mac";
+        UImanager->ApplyCommand(command);
+        
+        // Start the interactive session
         ui->SessionStart();
         delete ui;
     } else {
+        // Batch mode - execute the command provided as an argument
         G4String command = "/control/execute ";
         G4String fileName = argv[1];
         UImanager->ApplyCommand(command + fileName);
@@ -76,7 +84,8 @@ int main(int argc, char** argv) {
     // Job termination
     delete visManager;
     delete runManager;
-}
 
+    return 0;
+}
 
 
